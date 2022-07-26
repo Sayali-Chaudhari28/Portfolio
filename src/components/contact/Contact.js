30f828549e76ff9f77dca6674df811f3fd470d26
Sayalinc28 <sayalinc2001@gmail.com>
2022-07-26 23:39:50 +0530

    Added contact information and footer

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {AiOutlineLinkedin} from 'react-icons/ai'
 
 export const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__options'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>sayalinc2001@gmail.com</h5>
+          </article>
+          
+          <article className='contact__option'>
+            <AiOutlineLinkedin className='contact__option-icon'/>
+            <h4>LinkedIn</h4>
+            <a href='https://www.linkedin.com/in/sayali-chaudhari-2801/' target={'_blank'}>Connect with me on LinkedIn</a>
+          </article>
+        </div>
+      </div>
+      
+    </section>
   )
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import './footer.css'
+import {IoLogoTwitter} from 'react-icons/io'
+import { MdOutlineEmail } from 'react-icons/md'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      {/* <a href='#' className='footer__logo'></a> */}
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      {/* <div className='footer__socials'>
+        <a href='https://twitter.com/SayaliC03632508'><IoLogoTwitter /></a>
+      </div> */}
+
+      <div className='footer__copyright'>
+        <small>Made by Sayali</small>
+      </div>
+    </footer>
   )
 }
