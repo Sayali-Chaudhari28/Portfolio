37dff8372d02ff8e9751e98956a59e0809776ddc
Sayalinc28 <sayalinc2001@gmail.com>
2022-07-27 12:24:44 +0530

    Fixed warnings

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ export const Footer = () => {
       {/* <a href='#' className='footer__logo'></a> */}
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="/#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
