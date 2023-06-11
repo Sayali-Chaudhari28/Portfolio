3b55a1369e56b1b09e544e964ecc6a21351f99c2
dart_tool\Sayali-Chaudhari28 <sayalinc2001@gmail.com>
2023-06-11 18:30:44 +0530

    Updated UI of experience section

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,114 +1,101 @@
-import React from 'react'
-import './experience.css'
-import { BsPatchCheckFill } from 'react-icons/bs'
+import React from 'react';
+import './experience.css';
+import FLUTTER from '../../assets/flutter.png';
+import REACTJS from '../../assets/reactjs.png';
+import JAVA from '../../assets/java.png';
+import JAVASCRIPT from '../../assets/javascript.png';
+import HTML from '../../assets/html.png';
+import CSS from '../../assets/css.png';
+import ANDROID from '../../assets/android.png';
+import MYSQL from '../../assets/mysql.png';
+import MONGODB from '../../assets/mongodb.png';
+import FIREBASE from '../../assets/firebase.png';
 
 export const Experience = () => {
   return (
     <section id='experience'>
-      <h5> What Skills I Have</h5>
+      <h5>Skills I Have</h5>
       <h2>My Experience</h2>
 
       <div className='container experience__container'>
-        <div className='experience__frontend'>
-          <h3>Frontend Development</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
+        <div className='row'>
 
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>CSS</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>JavaScript</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>Boostrap</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>React</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
+          <div className='column'>
+            <div className='card'>
+              <img src={JAVA} alt='Java' />
+            </div>
+            <p>JAVA</p>
           </div>
+
+          <div className='column'>
+            <div className='card'>
+              <img src={FLUTTER} alt='Flutter' />
+            </div>
+            <p>FLUTTER</p>
+          </div>
+
+          <div className='column'>
+            <div className='card'>
+              <img src={REACTJS} alt='React' />
+            </div>
+            <p>REACT</p>
+          </div>
+
+          <div className='column'>
+            <div className='card'>
+              <img src={JAVASCRIPT} alt='Javascript' />
+            </div>
+            <p>JAVASCRIPT</p>
+          </div>
+
+          <div className='column'>
+            <div className='card'>
+              <img src={HTML} alt='Html' />
+            </div>
+            <p>HTML</p>
+          </div>
+
+          <div className='column'>
+            <div className='card'>
+              <img src={CSS} alt='CSS' />
+            </div>
+            <p>CSS</p>
+          </div>
+
+          <div className='column'>
+            <div className='card'>
+              <img src={ANDROID} alt='android' />
+            </div>
+            <p>ANDROID</p>
+          </div>
+
         </div>
+      </div>
 
-        <div className='experience__backend'>
-          <h3>Backend Development</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>Node JS</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
+      <div className='container experience__backend'>
+        <div className='row'>
+          <div className='column'>
+            <div className='card'>
+              <img src={MYSQL} alt='mysql' />
+            </div>
+            <p>MySQL</p>
+          </div>
 
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>MongoDB</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
+          <div className='column'>
+            <div className='card'>
+              <img src={MONGODB} alt='MongoDB' />
+            </div>
+            <p>MongoDB</p>
+          </div>
 
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>MySQL</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>Java</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            {/* <article className='experience__details'>
-              <BsPatchCheckFill />
-              <h4>JavaScript</h4>
-              <small className='text-light'>Experienced</small>
-            </article>
-
-            <article className='experience__details'>
-              <BsPatchCheckFill />
-              <h4>Boostrap</h4>
-              <small className='text-light'>Experienced</small>
-            </article>
-
-            <article className='experience__details'>
-              <BsPatchCheckFill />
-              <h4>React</h4>
-              <small className='text-light'>Experienced</small>
-            </article> */}
+          <div className='column'>
+            <div className='card'>
+              <img src={FIREBASE} alt='Firebase' />
+            </div>
+            <p>FIREBASE</p>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
